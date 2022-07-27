1a57ecbb8fa890de50b4770f713a53a6c22c2d6a
TarVics <tarvics@gmail.com>
2022-07-27 10:50:24 +0300

    Task4 output

--- js/main.js
+++ js/main.js
@@ -12,21 +12,11 @@ const styleNone = 'color: none;';
 const styleNumber = 'color: cornflowerblue;';
 const styleString = 'color: hotpink;';
 
-const headerBegin = `<p style="${styleHeader}">`, headerEnd = '</p>';
-const htmlBoolean = `<span style="${styleBoolean}">`;
-const htmlNumber = `<span style="${styleNumber}">`;
-const htmlString = `<span style="${styleString}">`;
-const htmlEOL = '</span><br>';
+// --[Task 1]-------------------------------------------
 
-const task1 = `- Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль`;
-const task2 = `- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.`;
-const task3 = `- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors.
-  Поле "автори" - масив. Кожен автор має поля name,age`;
-const task4 = `- Створити масив з 10 об'єктами які описують сутніть "користувач".
-Поля: name, username,password. Вивести в консоль пароль кожного користувача`;
+console.log('%c%s', styleHeader,
+    `- Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль`);
 
-// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
-console.log('%c%s', styleHeader, task1);
 const arrAnyData = [1, 2, 3, 'one', 'two', 'three', true, false, [1, 'two', true], {one: 1, two: true, three: 'three'}];
 console.log('arrAnyData[%c0%c] = %c%s', styleNumber, styleNone, styleNumber, arrAnyData[0]);
 console.log('arrAnyData[%c1%c] = %c%s', styleNumber, styleNone, styleNumber, arrAnyData[1]);
@@ -36,11 +26,15 @@ console.log('arrAnyData[%c4%c] = %c"%s"', styleNumber, styleNone, styleString, a
 console.log('arrAnyData[%c5%c] = %c"%s"', styleNumber, styleNone, styleString, arrAnyData[5]);
 console.log('arrAnyData[%c6%c] = %c%s', styleNumber, styleNone, styleBoolean, arrAnyData[6]);
 console.log('arrAnyData[%c7%c] = %c%s', styleNumber, styleNone, styleBoolean, arrAnyData[7]);
-console.log('arrAnyData[%c8%c] = %c%o', styleNumber, styleNone, arrAnyData[8]);
-console.log('arrAnyData[%c9%c] = %c%o', styleNumber, styleNone, arrAnyData[9]);
+console.log('arrAnyData[%c8%c] = %o', styleNumber, styleNone, arrAnyData[8]);
+console.log('arrAnyData[%c9%c] = %o', styleNumber, styleNone, arrAnyData[9]);
+console.log();
 
-// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.
-console.log('%c%s', styleHeader, task2);
+// --[Task 2]-------------------------------------------
+
+console.log('%c%s', styleHeader,
+    `- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre.`);
+
 const obj1 = {
     title: '«Код да Вінчі», Ден Браун',
     pageCount:  480,
@@ -59,10 +53,14 @@ const obj3 = {
 console.log(obj1);
 console.log(obj2);
 console.log(obj3);
+console.log();
 
-// - Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors.
-//   Поле "автори" - масив. Кожен автор має поля name,age
-console.log('%c%s', styleHeader, task3);
+// --[Task 3]-------------------------------------------
+
+console.log('%c%s', styleHeader,
+    `- Створити 3 об'єкти які описують книги. Поля об'єкту : title ,pageCount, genre, authors.
+  Поле "автори" - масив. Кожен автор має поля name,age`);
+
 const book1 = {
     title: '«Код да Вінчі», Ден Браун',
     pageCount: 480,
@@ -84,10 +82,13 @@ const book3 = {
 console.log(book1);
 console.log(book2);
 console.log(book3);
+console.log();
 
-// - Створити масив з 10 об'єктами які описують сутніть "користувач".
-//   Поля: name, username,password. Вивести в консоль пароль кожного користувача
-console.log('%c%s', styleHeader, task4);
+// --[Task 4]-------------------------------------------
+
+console.log('%c%s', styleHeader, `- Створити масив з 10 об'єктами які описують сутніть "користувач".
+  Поля: name, username,password. Вивести в консоль пароль кожного користувача`);
+
 const arrUsers = [
     {
         name: 'Адміністратор',
@@ -140,13 +141,13 @@ const arrUsers = [
         password: 'wsqaed'
     },
 ];
-console.log('%s = %c"%s"', arrUsers[0].name, styleString, arrUsers[0].password);
-console.log('%s = %c"%s"', arrUsers[1].name, styleString, arrUsers[1].password);
-console.log('%s = %c"%s"', arrUsers[2].name, styleString, arrUsers[2].password);
-console.log('%s = %c"%s"', arrUsers[3].name, styleString, arrUsers[3].password);
-console.log('%s = %c"%s"', arrUsers[4].name, styleString, arrUsers[4].password);
-console.log('%s = %c"%s"', arrUsers[5].name, styleString, arrUsers[5].password);
-console.log('%s = %c"%s"', arrUsers[6].name, styleString, arrUsers[6].password);
-console.log('%s = %c"%s"', arrUsers[7].name, styleString, arrUsers[7].password);
-console.log('%s = %c"%s"', arrUsers[8].name, styleString, arrUsers[8].password);
-console.log('%s = %c"%s"', arrUsers[9].name, styleString, arrUsers[9].password);
+console.log('arrUsers[%c%d%c].password = %c"%s"', styleNumber, 0, styleNone, styleString, arrUsers[0].password);
+console.log('arrUsers[%c%d%c].password = %c"%s"', styleNumber, 1, styleNone, styleString, arrUsers[1].password);
+console.log('arrUsers[%c%d%c].password = %c"%s"', styleNumber, 2, styleNone, styleString, arrUsers[2].password);
+console.log('arrUsers[%c%d%c].password = %c"%s"', styleNumber, 3, styleNone, styleString, arrUsers[3].password);
+console.log('arrUsers[%c%d%c].password = %c"%s"', styleNumber, 4, styleNone, styleString, arrUsers[4].password);
+console.log('arrUsers[%c%d%c].password = %c"%s"', styleNumber, 5, styleNone, styleString, arrUsers[5].password);
+console.log('arrUsers[%c%d%c].password = %c"%s"', styleNumber, 6, styleNone, styleString, arrUsers[6].password);
+console.log('arrUsers[%c%d%c].password = %c"%s"', styleNumber, 7, styleNone, styleString, arrUsers[7].password);
+console.log('arrUsers[%c%d%c].password = %c"%s"', styleNumber, 8, styleNone, styleString, arrUsers[8].password);
+console.log('arrUsers[%c%d%c].password = %c"%s"', styleNumber, 9, styleNone, styleString, arrUsers[9].password);
